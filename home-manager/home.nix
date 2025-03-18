33471d8343f6a19b21fa625aca84637414ebe694
thibautvas <thibaut.vas@gmail.com>
2025-03-18 18:38:44 +0100

    [cleanup] streamline and add basic packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,76 +1,44 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "thibautvas";
-  home.homeDirectory = "/home/thibautvas";
+  home = {
+    stateVersion = "24.11"; # should not be changed
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+    username = "thibautvas";
+    homeDirectory = "/home/thibautvas"; # linux standard
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    packages = with pkgs; [
+      vim
+      tmux
+      tree
+      fzf
+      fd
+      ripgrep
+    ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    sessionPath = [ "${config.home.homeDirectory}/.local/bin" ]; # prepend to path
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    sessionVariables.EDITOR = "vim";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/thibautvas/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+  programs = {
+    home-manager.enable = true; # let home manager manage itself
+    firefox.enable = true;
+    ghostty.enable = true;
+    zsh.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "thibautvas";
+      userEmail = "thibaut.vas@gmail.com";
+      extraConfig.init.defaultBranch = "main";
+      ignores = [
+        "temp.*"
+        "temp/"
+        "*.bak"
+        ".venv/"
+        "*.egg-info/"
+      ];
+    };
+  };
 }

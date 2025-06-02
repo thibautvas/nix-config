862a74cfef06ba580ca237c92b512f8c03f38639
thibautvas <thibaut.vas@gmail.com>
2025-06-02 19:35:49 +0200

    [vscode] sort settings and add git hunks keybindings

--- users/thibautvas/modules/vscode.nix
+++ users/thibautvas/modules/vscode.nix
@@ -9,74 +9,47 @@ in {
     package = unstablePkgs.vscode;
     profiles.default = {
       userSettings = {
-        "update.mode" = "none";
-        "window.restoreWindows" = "none";
-        "workbench.startupEditor" = "none";
-        "window.commandCenter" = false;
-        "workbench.editor.editorActionsLocation" = "hidden";
-        "workbench.layoutControl.enabled" = false;
-        "workbench.statusBar.visible" = false;
-        "workbench.activityBar.location" = "top";
-        "workbench.editor.showTabs" = "none";
-        "breadcrumbs.enabled" = false;
-        "editor.minimap.enabled" = false;
-        "editor.cursorBlinking" = "smooth";
-        "debug.toolBarLocation" = "docked";
-        "notebook.globalToolbar" = false;
-        "notebook.cellToolbarLocation"."default" = "hidden";
-        "editor.accessibilitySupport" = "off";
-        "workbench.colorCustomizations" = {
-          "activityBar.activeBackground" = "#004848";
-          "activityBar.background" = "#004848";
-          "activityBar.foreground" = "#e7e7e7";
-          "activityBar.inactiveForeground" = "#e7e7e799";
-          "activityBarBadge.background" = "#000000";
-          "activityBarBadge.foreground" = "#e7e7e7";
-          "commandCenter.border" = "#e7e7e799";
-          "sash.hoverBorder" = "#004848";
-          "statusBar.background" = "#001515";
-          "statusBar.foreground" = "#e7e7e7";
-          "statusBarItem.hoverBackground" = "#004848";
-          "statusBarItem.remoteBackground" = "#001515";
-          "statusBarItem.remoteForeground" = "#e7e7e7";
-          "titleBar.activeBackground" = "#001515";
-          "titleBar.activeForeground" = "#e7e7e7";
-          "titleBar.inactiveBackground" = "#00151599";
-          "titleBar.inactiveForeground" = "#e7e7e799";
-        };
-        "workbench.colorTheme" = "Catppuccin Macchiato";
-        "workbench.editor.enablePreview" = false;
-        "editor.cursorSurroundingLines" = 10;
-        "editor.snippetSuggestions" = "top";
-        "files.insertFinalNewline" = true;
-        "editor.autoClosingQuotes" = "never";
-        "editor.autoClosingBrackets" = "never";
         "[python]" = {
           "editor.formatOnSave" = true;
           "editor.defaultFormatter" = "charliermarsh.ruff";
           "editor.codeActionsOnSave"."source.fixAll" = "explicit";
         };
-        "notebook.output.textLineLimit" = 40;
+        "breadcrumbs.enabled" = false;
+        "editor.accessibilitySupport" = "off";
+        "editor.autoClosingBrackets" = "never";
+        "editor.autoClosingQuotes" = "never";
+        "editor.cursorBlinking" = "smooth";
+        "editor.cursorSurroundingLines" = 10;
+        "editor.minimap.enabled" = false;
+        "extensions.ignoreRecommendations" = true;
+        "files.exclude"."**/.git" = false;
+        "files.insertFinalNewline" = true;
+        "git.openRepositoryInParentFolders" = "never";
+        "jupyter.askForKernelRestart" = false;
+        "jupyter.interactiveWindow.codeLens.enable" = false;
         "jupyter.interactiveWindow.textEditor.executeSelection" = true;
         "jupyter.interactiveWindow.viewColumn" = "beside";
+        "notebook.cellToolbarLocation"."default" = "hidden";
+        "notebook.globalToolbar" = false;
+        "notebook.output.textLineLimit" = 40;
         "python.terminal.activateEnvironment" = false;
-        "workbench.editor.empty.hint" = "hidden";
         "terminal.integrated.initialHint" = false;
-        "extensions.ignoreRecommendations" = true;
-        "git.openRepositoryInParentFolders" = "never";
-        "files.exclude"."**/.git" = false;
-        "jupyter.askForKernelRestart" = false;
-        "vim.smartRelativeLine" = true;
+        "update.mode" = "none";
+        "vim.highlightedyank.enable" = true;
         "vim.mouseSelectionGoesIntoVisualMode" = false;
         "vim.overrideCopy" = false;
-        "vim.highlightedyank.enable" = true;
-        "sqltools.autoOpenSessionFiles" = false;
-        "sqltools.highlightQuery" = false;
-        "sqltools.queryParams.enableReplace" = true;
-        "sqltools.queryParams.regex" = "\\{([\\d\\w]+)\\}|\\$\\{([\\d\\w]+)\\}|<\\[([\\d\\w\\.\\s\\[\\]]+)\\]>";
-        "sqltools.results.location" = "next";
-        "sqltools.defaultOpenType" = "csv";
-        "sqltools.defaultExportType" = "csv";
+        "vim.smartRelativeLine" = true;
+        "window.commandCenter" = false;
+        "window.restoreWindows" = "none";
+        "workbench.activityBar.location" = "top";
+        "workbench.colorTheme" = "Catppuccin Macchiato";
+        "workbench.editor.editorActionsLocation" = "hidden";
+        "workbench.editor.empty.hint" = "hidden";
+        "workbench.editor.enablePreview" = false;
+        "workbench.editor.showTabs" = "none";
+        "workbench.layoutControl.enabled" = false;
+        "workbench.startupEditor" = "none";
+        "workbench.statusBar.visible" = false;
       };
 
       keybindings = [
@@ -127,6 +100,21 @@ in {
           key = "${cmdCtrl}+e ${cmdCtrl}+f";
           command = "sqltools.executeCurrentQuery";
           when = "editorTextFocus && editorLangId == 'sql' && !config.sqltools.disableChordKeyBindings";
+        }
+	{
+          key = "alt+h";
+          command = "workbench.action.editor.nextChange";
+          when = "editorTextFocus && !textCompareEditorActive && quickDiffDecorationCount != '0'";
+        }
+	{
+          key = "alt+shift+h";
+          command = "workbench.action.editor.previousChange";
+          when = "editorTextFocus && !textCompareEditorActive && quickDiffDecorationCount != '0'";
+        }
+	{
+          key = "ctrl+shift+h";
+          command = "git.stageSelectedRanges";
+          when = "editorTextFocus && !operationInProgress && resourceScheme == 'file'";
         }
       ];
     };

0cec80b94048d7178dd91116daaa0692a74c48b7
thibautvas <thibaut.vas@gmail.com>
2025-06-02 20:11:17 +0200

    [temp:nvim] telescope bug: revert to nvim 0.11.1

--- users/thibautvas/modules/neovim/default.nix
+++ users/thibautvas/modules/neovim/default.nix
@@ -1,6 +1,15 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  # lock nvim 0.11.1
+  oldPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/12a55407652e04dcf2309436eb06fef0d3713ef3.tar.gz";
+    sha256 = "sha256-N4cp0asTsJCnRMFZ/k19V9akkxb7J/opG+K+jU57JGc=";
+  }) {
+    inherit (pkgs) system;
+  };
+
+in {
   # new imports are prepended to init.lua
   # settings.nix is always sourced last to put the mapleader at the top of init.lua
   imports = [
@@ -15,6 +24,7 @@
 
   programs.neovim = {
     enable = true;
+    package = oldPkgs.neovim-unwrapped;
     defaultEditor = true;
     vimdiffAlias = true;
   };

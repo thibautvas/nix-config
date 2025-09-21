2755a9a128912efb411e7fb3370cc8a407ee4612
thibautvas <thibaut.vas@gmail.com>
2025-09-21 15:50:34 +0800

    [kmonad] merge nav and com layers

--- users/thibautvas/modules/kmonad.nix
+++ users/thibautvas/modules/kmonad.nix
@@ -65,43 +65,31 @@ let
       vim_V #(${start} S-${end})
       vim_S #(${end} S-${start} bspc)
       vim_o #(${end} S-ret)
-
-      ;; comfort layer: email addresses and accessible controls for 60% keyboard
-      com (layer-toggle com)
-      com_tab (tap-hold-next-release 200 tab @com)
       gmail #(t h i b a u t . v a s @ g m a i l . c o m)
     )
 
     (defsrc
       esc       f1        f2        f3        f4        f5        f6        f7        f8        f9        f10       f11       f12
-                1         2         3         4         5         6         7         8         9         0         -         =         bspc
-      tab                                                         y         u         i         o         p         [         ]
-      caps      a         s         d         f                   h         j         k         l         ;
-                                                                            m         ,         .
+                1         2         3                   5         6         7         8         9         0         -         =         bspc
+                                                                  y         u         i         o         p         [         ]
+      caps      a         s         d         f                   h         j         k         l         ;         '
+                                    c         v                             m         ,         .
     )
 
     (deflayer mod
       grv       brdn      brup      mctl      _         bldn      blup      prev      pp        next      mute      vold      volu
-                _         _         _         _         _         _         _         _         _         _         _         _         _
-      @com_tab                                                    _         _         _         _         _         _         _
-      @nav_esc  @alt_a    @hym_s    @sft_d    @ctl_f              _         @ctl_j    @sft_k    @met_l    @alt_;
-                                                                            _         _         _
+                _         _         _                   _         _         _         _         _         _         _         _         _
+                                                                  _         _         _         _         _         _         _
+      @nav_esc  @alt_a    @hym_s    @sft_d    @ctl_f              _         @ctl_j    @sft_k    @met_l    @alt_;    _
+                                    _         _                             _         _         _
     )
 
     (deflayer nav
-      _         f1        f2        f3        f4        f5      f6          f7        f8        f9        f10       f11       f12
-                _         _         _         _         _       _           S-@vim_b  S-@vim_w  @vim_V    @vim_S    _         _         del
-      _                                                         @vim_y      @vim_b    @vim_w    @vim_o    @vim_p    _         _
-      _         _         _         @vim_d    _                 left        down      up        right     _
-                                                                            @vim_^    @vim_$    S-down
-    )
-
-    (deflayer com
-      _         _         _         _         _         _         _         _         _         _         _         _         _
-                brdn      brup      mctl      _         bldn      blup      prev      pp        next      mute      vold      volu      del
-      _                                                           _         _         _         _         -         _         @gmail
-      caps      _         _         _         _                   _         _         _         _         _
-                                                                            _         _         _
+      _         f1        f2        f3        f4        f5        f6        f7        f8        f9        f10       f11       f12
+                brdn      brup      mctl                bldn      blup      prev      pp        next      mute      vold      volu      del
+                                                                  @vim_y    @vim_b    @vim_w    @vim_o    @vim_p    S-@vim_b  S-@vim_w
+      _         _         @vim_S    @vim_d    _                   left      down      up        right     _         @gmail
+                                    caps      @vim_V                        @vim_^    @vim_$    S-down
     )
   '';
 

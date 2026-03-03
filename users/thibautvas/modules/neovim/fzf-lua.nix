442fe3c5d03d59b7966eca0dc380ac8bc1c5fe6d
thibautvas <thibaut.vas@gmail.com>
2026-03-03 04:32:22 +0100

    [nvim] fzf-lua project picker

--- users/thibautvas/modules/neovim/fzf-lua.nix
+++ users/thibautvas/modules/neovim/fzf-lua.nix
@@ -27,11 +27,29 @@
       vim.keymap.set("n", "<leader>fd", fl.files, { desc = "FzfLua files" })
       vim.keymap.set("n", "<leader>ff", fl.buffers, { desc = "FzfLua buffers" })
       vim.keymap.set("n", "<leader>fs", fl.git_status, { desc = "FzfLua git status" })
-      vim.keymap.set("n", "<leader>fa", function()
-        fl.files { cwd = vim.fn.getenv("WORK_DIR") }
-      end,
-      { desc = "FzfLua WORK_DIR files" })
       vim.keymap.set("n", "<leader>fg", fl.live_grep_native, { desc = "FzfLua live grep" })
+
+      local work_dir = vim.fn.getenv("WORK_DIR")
+      vim.keymap.set("n", "<leader>fa", function()
+        fl.files { cwd = work_dir }
+      end, { desc = "FzfLua WORK_DIR files" })
+
+      vim.keymap.set("n", "<leader>fp", function()
+        fl.fzf_exec("fd -t d -d 1 -L --base-directory " .. work_dir, {
+          winopts = { title = " Projects " },
+          actions = {
+            ["default"] = function(selected)
+              if selected[1] then
+                local dir = table.concat({
+                  work_dir,
+                  vim.fn.fnameescape(selected[1])
+                }, "/")
+                vim.cmd("cd " .. dir)
+              end
+            end,
+          },
+        })
+      end, { desc = "FzfLua change directory" })
     '';
   };
 }
